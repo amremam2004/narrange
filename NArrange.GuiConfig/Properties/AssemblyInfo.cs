b5a6670f7bdd22265e8d0d4e9979fa3eb66041c6
jamesnies <jamesnies@910f5c13-0943-0410-a66e-e02167cd1f8f>
2008-06-01 21:10:21 +0000

    Use common assembly info for the configuration application.

--- NArrange.GuiConfig/Properties/AssemblyInfo.cs
+++ NArrange.GuiConfig/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NArrange.GuiConfig")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NArrange.GuiConfig")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,13 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("37987cb9-0933-49d7-8853-85a055cda006")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
