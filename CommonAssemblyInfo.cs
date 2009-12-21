f6535f5d913c8f2d61d6b340ecbcee64b5155bb1
jamesnies <jamesnies@910f5c13-0943-0410-a66e-e02167cd1f8f>
2009-12-21 18:56:59 +0000

    Bumped version number

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.2.9")]
-[assembly: AssemblyFileVersion("0.2.9")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
